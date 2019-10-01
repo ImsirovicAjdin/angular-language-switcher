c25c90237452ec4c9955bec6f828fdf7a32c0ea2
ImsirovicAjdin <ajdika@live.com>
2019-10-01 13:48:20 +0200

    There's nothing better than optimized code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
+export class AppComponent implements OnInit {
   public lang = localStorage.setItem('lang', 'en');
 
+  ngOnInit() {
+    document.onkeyup = (e) => {
+      if (e.ctrlKey && e.altKey && e.key === 'c') {
+        this.switchLang();
+        console.log(this.switchLanguage);
+      }
+    };    
+  }
 
 
   public switchLanguage = 'en';
@@ -37,16 +45,3 @@ export class AppComponent  {
 
 }
 
-    // document.onkeyup = function(e) {
-    //   if (e.ctrlKey && e.key === 'c') { // CTRL + c
-    //     const lang = sessionStorage.getItem('lang');
-
-    //     if (lang === 'en') {
-    //       translate.use('it');
-    //       sessionStorage.setItem('lang', 'it');
-    //     } else {
-    //       translate.use('en');
-    //       sessionStorage.setItem('lang', 'en');
-    //     }
-    //   }
-    // };
